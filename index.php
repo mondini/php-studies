196e57c799f01f46675d5846b7752dd930e3d5aa
bamgabsz <bamgabsz@gmail.com>
2023-01-01 20:14:43 -0300

    exercicio positivo e negativo

--- index.php
+++ index.php
@@ -9,11 +9,32 @@
 
 </head>
 
-    <body>
-    <!-- Faça um programa que mostre a mensagem "Alo mundo" na tela. -->
-        <?php
-        echo "Hello World"; 
-        ?>
-    </body>
+<body>
+    <!-- Crie um algoritmo que receba um número digitado pelo usuário e verifique se esse valor é positivo, 
+    negativo ou igual a zero. A saída deve ser: "Valor Positivo", "Valor Negativo" ou "Igual a Zero" -->
+
+    <h3>Esse Sistema irá te mostrar se o valor digitado é positivo ou negativo</h3>
+    <form action="" method="POST">
+        Digite um número: <input type="number" name="number">
+        <input type="submit" name="enviar"><br>
+    </form>
+    <?php
+        $number = $_POST['number'];
+
+        function numberIsPositive($number){
+            if($number >= 0){
+                echo "O número ",$number, " é positivo!";
+            } else {
+                echo "O número ",$number, " é negativo!";
+            };
+        }
+        
+        numberIsPositive($number);
+        
+    ?>
+
+</body>
+
+<script></script>
 
 </html>
