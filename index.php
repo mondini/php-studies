4a81fd7050ff0052ed240ac6d946da2aee869aac
bamgabsz <bamgabsz@gmail.com>
2023-01-01 20:25:21 -0300

    exercicio tabuada

--- index.php
+++ index.php
@@ -10,31 +10,26 @@
 </head>
 
 <body>
-    <!-- Crie um algoritmo que receba um número digitado pelo usuário e verifique se esse valor é positivo, 
-    negativo ou igual a zero. A saída deve ser: "Valor Positivo", "Valor Negativo" ou "Igual a Zero" -->
+<!-- Crie um algoritmo que solicite a entrada de um número, e exiba a tabuada de 0 a 10 de acordo com o número solicitado, ex: 
+Entrada = 4
+Saída = 4 X 0 = 0...4 X 10 = 40. -->
 
-    <h3>Esse Sistema irá te mostrar se o valor digitado é positivo ou negativo</h3>
-    <form action="" method="POST">
-        Digite um número: <input type="number" name="number">
-        <input type="submit" name="enviar"><br>
-    </form>
-    <?php
-        $number = $_POST['number'];
+<form action="" method="POST">
+    Digite um Numero: <input type="number" name="number">
+    <input type="Submit" name="Enviar">
+</form>
 
-        function numberIsPositive($number){
-            if($number >= 0){
-                echo "O número ",$number, " é positivo!";
-            } else {
-                echo "O número ",$number, " é negativo!";
-            };
+<?php
+    $number = $_POST['number'];
+
+    function multiplicationTable($number){
+        for ($i=0; $i < 11; $i++) { 
+            echo $number, "x", $i, "=", $number * $i, "<br>"; 
         }
-        
-        numberIsPositive($number);
-        
-    ?>
+    }
 
+    multiplicationTable($number);
+?>
 </body>
-
-<script></script>
 
 </html>
