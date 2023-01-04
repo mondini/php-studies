9f528c37ad14febfb5e1355f94ec15d7ae45effa
bamgabsz <bamgabsz@gmail.com>
2023-01-04 11:24:19 -0300

    exec consumo medio combustivel

--- index.php
+++ index.php
@@ -10,27 +10,26 @@
 </head>
 
 <body>
-    <!-- Faça um programa que calcule a quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz 12Km por litro. Para obter o cálculo, o usuário deve fornecer o tempo gasto na viagem e a velocidade média durante ela. Desta forma, será possível obter a distância percorrida com a fórmula DISTANCIA = TEMPO × VELOCIDADE. Tendo o valor da distância, basta calcular a quantidade de litros de combustível utilizada na viagem com a fórmula: LITROS_USADOS = DISTANCIA ÷ 12. O programa deve apresentar os valores da velocidade média, tempo gasto na viagem, a distância percorrida e a quantidade de litros utilizada na viagem.
+    <!-- Faça um programa para determinar o consumo médio de um automóvel sendo fornecida a distância total percorrida pelo automóvel e o total de combustível gasto.
 
 -->
     <form action="./index.php" method="POST">
-        Digite o tempo gasto na viagem: <input type="number" name="temp">
-        Digite a velocidade média durante ela: <input type="number" name="velocity">
+        Digite a distancia total percorrida: <input type="number" name="distancia"><br>
+        Digite o total de combustível gasto: <input type="number" name="combustivel">
         <input type="submit" name="Enviar">
     </form>
 
     <?php
-        $temp = $_POST['temp'];
-        $velocity = $_POST['velocity'];
+        $distancia = $_POST['distancia'];
+        $combustivel = $_POST['combustivel'];
 
-        function calcularQntdLitros ($tempo, $velocidade){
-            $distancia = $tempo * $velocidade;
-            $litrosUsados = $distancia / 12;
+        function calcularQntdLitros ($distancia, $combustivel){
+            $consumoMédio = $distancia / $combustivel;
 
-            echo $litrosUsados;
+            echo $consumoMédio."km";
         }
 
-        calcularQntdLitros($temp, $velocity);
+        calcularQntdLitros($distancia, $combustivel);
     ?>
 </body>
 
