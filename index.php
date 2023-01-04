11c2122effd70ed070262442180f85a2bda68b03
bamgabsz <bamgabsz@gmail.com>
2023-01-04 09:36:01 -0300

    exec quantidade Litros

--- index.php
+++ index.php
@@ -10,22 +10,27 @@
 </head>
 
 <body>
-    <!-- Faça um programa que leia uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit. A fórmula de conversão é: ℉ = (9 × ℃ + 160) ÷ 5, na qual ℉ é a temperatura em Fahrenheit e ℃ é a temperatura em Celsius.-->
+    <!-- Faça um programa que calcule a quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz 12Km por litro. Para obter o cálculo, o usuário deve fornecer o tempo gasto na viagem e a velocidade média durante ela. Desta forma, será possível obter a distância percorrida com a fórmula DISTANCIA = TEMPO × VELOCIDADE. Tendo o valor da distância, basta calcular a quantidade de litros de combustível utilizada na viagem com a fórmula: LITROS_USADOS = DISTANCIA ÷ 12. O programa deve apresentar os valores da velocidade média, tempo gasto na viagem, a distância percorrida e a quantidade de litros utilizada na viagem.
+
+-->
     <form action="./index.php" method="POST">
-        <h3>Conversão de Celsius para Fahrenheit</h3>
-        Digite uma temperatura: <input type="number" name="temp">
+        Digite o tempo gasto na viagem: <input type="number" name="temp">
+        Digite a velocidade média durante ela: <input type="number" name="velocity">
         <input type="submit" name="Enviar">
     </form>
-    
+
     <?php
         $temp = $_POST['temp'];
+        $velocity = $_POST['velocity'];
 
-        function celsiusToFahrenheit ($temp) {
-            $result = (1.8 * $temp) + 32;
-            echo $result;
+        function calcularQntdLitros ($tempo, $velocidade){
+            $distancia = $tempo * $velocidade;
+            $litrosUsados = $distancia / 12;
+
+            echo $litrosUsados;
         }
 
-        celsiusToFahrenheit($temp);
+        calcularQntdLitros($temp, $velocity);
     ?>
 </body>
 
