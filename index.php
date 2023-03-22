72c683ff18be886067158d5c175eeac2fc0e3783
bamgabsz <bamgabsz@gmail.com>
2023-03-22 15:38:19 -0300

    ALURA - primeiros passos

--- index.php
+++ index.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Estudos</title>
-
-</head>
-
-<body>
-
-    <?php
-
-        function echo() {
-            return true;
-        }
-
-        #Não funciona
-
-        function echoNew() {
-            return true;
-        }
-        
-        #funciona
-    ?>
-</body>
-
-</html>
+echo "Olá Mundo"; 
